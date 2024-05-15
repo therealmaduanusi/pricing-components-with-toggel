2bbe39d6d51ab1a9c862cbf90b7bd56b6ddab271
Victor Maduanusi <maduanusivictor2020@gmail.com>
2024-05-16 00:45:23 +0100

    toggle classlist add and remove

--- script.js
+++ script.js
@@ -9,12 +9,14 @@ let intMoney2 = parseFloat(money2.innerText) + 225
 let intMoney3 = parseFloat(money3.innerText) + 360
 
 toggleState.addEventListener('click', () => {
-    let toggles = toggleState.classList.toggle('toggle-state')
+    let toggles = toggleState.classList.toggle('toggle-state');
     if (toggles) {
+        toggleState.classList.remove('toggle');
         money1.innerText = intMoney1.toFixed(2);
         money2.innerText = intMoney2.toFixed(2);
         money3.innerText = intMoney3.toFixed(2);
     } else {
+        toggleState.classList.add('toggle');
         money1.innerText = '19.99';
         money2.innerText = '24.99';
         money3.innerText = '39.99';
